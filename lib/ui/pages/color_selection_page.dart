b6f97ea8890375240f1c704f061148264b5b0e48
Marcela Hernandez <vmhernandez@uninorte.edu.co>
2022-08-15 22:32:24 -0500

    Adding Colors

--- lib/ui/pages/color_selection_page.dart
+++ lib/ui/pages/color_selection_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter/services.dart';
-
 import '../../utils/color_utils.dart';
 import '../widgets/color_palette.dart';
 
@@ -25,10 +24,43 @@ class ColorSelectionPage extends StatelessWidget {
           ],
         ),
         // https://colorhunt.co/
-        body: (const Center(
-          child: Text(
-              "SingleChildScrollView -> Column -> [ColorPalette, ColorPalette] "),
-        )));
+        body: SingleChildScrollView(
+          child: Column(children: [
+            ColorPalette(
+              callback: showColor,
+              baseColor: 'C21010',
+              baseColor1: 'E64848',
+              baseColor2: 'FFFDE3',
+              baseColor3: 'CFE8A9',
+              iconName: Image.network(
+                  'https://static.vecteezy.com/system/resources/previews/004/930/349/original/strawberry-fruit-line-for-web-presentation-logo-icon-symbol-free-vector.jpg'),
+            ),
+            ColorPalette(
+                callback: showColor,
+                baseColor: 'FFD24C',
+                baseColor1: 'FFE69A',
+                baseColor2: 'FFE59D',
+                baseColor3: 'F1F0C0',
+                iconName: Image.network(
+                    'https://previews.123rf.com/images/mimacz/mimacz1707/mimacz170700105/82349424-fruta-de-lim%C3%B3n-logo-dise%C3%B1o-icono-simple.jpg')),
+            ColorPalette(
+                callback: showColor,
+                baseColor: '371B58',
+                baseColor1: '4C3575',
+                baseColor2: '5B4B8A',
+                baseColor3: '7858A6',
+                iconName: Image.network(
+                    'https://static.vecteezy.com/system/resources/thumbnails/002/098/807/small_2x/cartoon-eggplant-icon-aubergine-symbol-fresh-vegetarian-salad-recipe-purple-colored-nutrition-foods-flat-cartoon-style-isolated-vegetable-clip-art-illustration-vector.jpg')),
+            ColorPalette(
+                callback: showColor,
+                baseColor: 'FFD24C',
+                baseColor1: 'FFE69A',
+                baseColor2: 'FFE59D',
+                baseColor3: 'F1F0C0',
+                iconName: Image.network(
+                    'https://previews.123rf.com/images/mimacz/mimacz1707/mimacz170700105/82349424-fruta-de-lim%C3%B3n-logo-dise%C3%B1o-icono-simple.jpg')),
+          ]),
+        ));
   }
 
   // está es la función que será llamada con cada click a un ColorPalette
--- lib/ui/widgets/color_palette.dart
+++ lib/ui/widgets/color_palette.dart
@@ -2,27 +2,77 @@ import 'package:flutter/material.dart';
 import '../../utils/color_utils.dart';
 
 class ColorPalette extends StatelessWidget {
-  const ColorPalette({
-    Key? key,
-    // así se reciben en el constructor, por ejemplo
-    //required this.baseColor,
-  }) : super(key: key);
+  const ColorPalette(
+      {Key? key,
+
+      // así se reciben en el constructor, por ejemplo
+      //required this.baseColor,
+      required this.callback,
+      required this.baseColor,
+      required this.baseColor1,
+      required this.baseColor2,
+      required this.baseColor3,
+      required this.iconName})
+      : super(key: key);
 
   // atributos de la clase, por ejemplo
   //final String baseColor;
 
+  final String baseColor;
+  final String baseColor1;
+  final String baseColor2;
+  final String baseColor3;
+  final Widget iconName;
   // también se puede tener como un atributo un callback, necesiario ya que es un
   // widget sin estado, por ejemplo
-  //final Function(String) callback;
+  final Function(String) callback;
 
   @override
   Widget build(BuildContext context) {
     // padding para crear espacio alrededor del widget
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       // vamos a mostrar los elementos en un row
-      child: Text(
-          'Row -> [Expanded(flex: 4,GestureDetector(Container(height: 100, color: ColorUtils.FromHex(baseColor))) )]'),
+      child: Row(children: [
+        Expanded(
+          flex: 4,
+          child: GestureDetector(
+            onTap: () => callback(baseColor),
+            child: Container(height: 100, color: ColorUtils.FromHex(baseColor)),
+          ),
+        ),
+        Expanded(
+          flex: 4,
+          child: GestureDetector(
+            onTap: () => callback(baseColor1),
+            child:
+                Container(height: 100, color: ColorUtils.FromHex(baseColor1)),
+          ),
+        ),
+        Expanded(
+          flex: 4,
+          child: GestureDetector(
+            onTap: () => callback(baseColor2),
+            child:
+                Container(height: 100, color: ColorUtils.FromHex(baseColor2)),
+          ),
+        ),
+        Expanded(
+          flex: 4,
+          child: GestureDetector(
+            onTap: () => callback(baseColor3),
+            child:
+                Container(height: 100, color: ColorUtils.FromHex(baseColor3)),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: iconName,
+          ),
+        ),
+      ]),
     );
   }
 }
